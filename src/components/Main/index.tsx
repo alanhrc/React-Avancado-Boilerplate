01c6e70a76286f2c293be0dc027a4b4c384d05a8
Alan <alancamargo50@gmail.com>
2021-02-09 16:35:35 -0300

    add styled-components, storybook and configure

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,22 @@
-const Main = () => {
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, NextJS e Styled Components',
+}) => {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado"
+      />
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para a tela do código"
+      />
+    </S.Wrapper>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import GlobalStyle from '../styles/GlobalStyle'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="A simple project started to workwith typescript and Styled Components"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  )
 }
 
 export default MyApp
